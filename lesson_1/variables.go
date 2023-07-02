456398e0f39e5b269fbe73fb195c1538b3e5ee1d
kusakabeka <n.kuzmingg@gmail.com>
2023-07-02 20:00:52 +0300

    lesson 1

--- lesson_1/variables.go
+++ lesson_1/variables.go
@@ -1,6 +1,16 @@
 package main
 
-func main() {
-	// Переменные
+import "fmt"
 
+func main() {
+
+	var message = "123"
+	var number float32
+	var b bool
+
+	number = 12.2
+
+	fmt.Println(message)
+	fmt.Println(number)
+	fmt.Println(b)
 }
