8616dcdc679bd1e58f6d21ee909b5cd7cac36055
kusakabeka <n.kuzmingg@gmail.com>
2023-07-09 10:37:50 +0300

    lesson 6

--- maps.go
+++ maps.go
@@ -3,20 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	users := map[string]int{
+	user := map[string]int{
 		"name_1": 15,
 		"name_2": 24,
 		"name_3": 55,
 		"name_4": 5,
 	}
-	// fmt.Println(user["name_5"]) -> return 0
-	age, ok := users["name_1"]
+	fmt.Println(user)
+	// users :=[]string{"1", "2"}
+	//users := make(map[string]int)
+	//user["Ivan"] = 12
+	//for key, value := range users {
+	//	fmt.Println(key, value)
+	//}
+
+	// fmt.Println(user[""]) -> return 0
+	age, ok := user["name_1"]
 	fmt.Println(age, ok)
 
-	users["name_5"] = 21
+	user["name_5"] = 21
 
-	delete(users, "name_2")
-	for key, value := range users {
-		fmt.Println(key, value)
-	}
+	delete(user, "name_2")
+
+	fmt.Println(len(user))
+	// cap() - не работает с мапами
+
 }
